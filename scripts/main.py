adca09bad1832a610e9e2e5bece9cef8fb138a37
Tomy Doda <tomy.doda@gmail.com>
2024-11-07 12:22:27 +0100

    Modification of main.py: add comments

--- scripts/main.py
+++ scripts/main.py
@@ -6,6 +6,7 @@ import yaml
 from adcp import ADCP
 from functions import latest_files, all_files, log, select_parameters
 
+
 with open("scripts/input_python.yaml", "r") as f:
     directories = yaml.load(f, Loader=yaml.FullLoader) # Path to the data directories L0, L1, L2
     
@@ -31,10 +32,10 @@ else:
 for file in files_300:
     a = ADCP()
     p = select_parameters(file, parameter_dict) # Get the parameters corresponding to the specific deployment period
-    if a.read_data(file, transducer_depth=p["transducer_depth"], bottom_depth=p["bottom_depth"], up=p["up"]): # Read the raw data
+    if a.read_data(file, transducer_depth=p["transducer_depth"], bottom_depth=p["bottom_depth"], up=p["up"]): # Read the raw data  
         a.quality_flags("./quality_assurance.json") # Flag the data based on quality checks
         a.export(os.path.join(directories["Level1_dir"], "RDI300"), "L1", output_period="file", overwrite_file=True) # Create Level 1 file
-        a.mask_data() # Apply a mask on the flagged data
+        a.mask_data() # Replace flagged data by nan 
         a.derive_variables(p["rotate_velocity"]) # Compute additional variables to add to Level 2
         a.export(os.path.join(directories["Level2_dir"], "RDI300"), "L2", output_period="file", overwrite_file=True) # Create Level 2 file
 
@@ -42,6 +43,7 @@ for file in files_600:
     a = ADCP()
     p = select_parameters(file, parameter_dict)
     if a.read_data(file, transducer_depth=p["transducer_depth"], bottom_depth=p["bottom_depth"], up=p["up"]):
+        breakpoint()
         a.quality_flags("./quality_assurance.json")
         a.export(os.path.join(directories["Level1_dir"], "RDI600"), "L1", output_period="file", overwrite_file=True)
         a.mask_data()
