79a4f39062b241cec7f3e6a60d2d35ad8777d85f
Tomy Doda <tomy.doda@gmail.com>
2025-07-10 14:50:09 +0200

    Adapt .bat files to process all Level0 data each time they are run

--- run_main_TD.bat
+++ run_main_TD.bat
@@ -1,4 +1,30 @@
+:: UTF-8 characters (accents):
+chcp 65001
 
 @echo off
-C:\Users\tdoda\AppData\Local\anaconda3\envs\datalakes\python.exe scripts\main_TD.py
+setlocal enabledelayedexpansion
+
+:: Ensure correct location
+:: cd "C:\Users\Seatronic 1147\Documents\Data_Lexplore\git\acoustic-doppler-current-profiler
+
+:: Load input variables
+call "scripts\input_batch_TD.bat"
+
+:: Backup files
+:: robocopy %in300% %raw300% *.LTA /E /MIR
+:: robocopy %in600% %raw600% *.LTA /E /MIR
+
+:: Process ADCP data
+:: To process the most recent data only:
+::%pythonenv% %script% live
+
+:: To process everything:
+%pythonenv% %script%
+echo Data has been processed
 pause
+
+:: %pythonenv% %upload% -w
+
+:: curl "https://api.datalakes-eawag.ch/update/599"
+:: timeout 20
+:: curl "https://api.datalakes-eawag.ch/update/600"
